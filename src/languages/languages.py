71bfc12b22b8d451af8b0171cbe5bfd8138d00b6
kcitlyn <kcitlynychen@gmail.com>
2025-07-19 19:41:52 -0500

    adding langauges

---
+++ src/languages/languages.py
@@ -0,0 +1,31 @@
+from vosk import Model, KaldiRecognizer
+from argostranslate import translate, package
+import os
+import requests
+
+class LanguageManager:
+    data = {
+    #"language": ["language code", Model("path to language in files"), model url for installation (this is for vosk transcription)]
+    #in order to find pathway to file, locate file and open in terminal; then type pwd and copy paste into below section
+    "english": ["en", "/Users/kaitlynchen/rsp5/translator/src/vosk_models/vosk-model-en-us-0.22-lgraph"],
+    "mandarin": ["zh", "/Users/kaitlynchen/rsp5/translator/src/vosk_models/vosk-model-small-cn-0.22"],
+    "cantonese": ["zh", "/Users/kaitlynchen/rsp5/translator/src/vosk_models/vosk-model-small-cn-0.22"], #double check its the same 
+    "chinese": ["zh", "/Users/kaitlynchen/rsp5/translator/src/vosk_models/vosk-model-small-cn-0.22"],
+    "spanish": ["es"]
+    #add specific languages to ur preferances
+    }
+
+    @staticmethod
+    def get_code(lang):
+        code = LanguageManager.data.get(lang, [None, None])[0]
+        if code is None:
+             raise ValueError(f"No code was found for language '{lang}'. Look at LibreTranslate codes for more information.")
+        return code
+    
+    @staticmethod
+    def get_model(lang):
+        model = LanguageManager.data.get(lang, [None, None])[1]
+        if model is None:
+            raise ValueError(f"No model was found for language '{lang}'. Look at LibreTranslate models for more information.")
+        return Model(model)
+    
